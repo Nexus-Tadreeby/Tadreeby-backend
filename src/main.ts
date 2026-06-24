d9b4c9b2bd1332bf9f477ed305e9c954e31b9e62
shahd <eng.shahd.abusharif@gmail.com>
2026-06-24 16:58:17 +0300

    enable CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.setGlobalPrefix('api');
-
-  
+  app.enableCors({
+    origin: [
+      'http://localhost:5173/',
+      'https://tadreeby.vercel.app/',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Tadreeby API')
