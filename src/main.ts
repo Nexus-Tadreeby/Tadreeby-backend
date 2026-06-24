03f95f186a5d90205f27a7aa2f7a22e759631014
shahd <eng.shahd.abusharif@gmail.com>
2026-06-24 16:42:58 +0300

    remove global prefix

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
 
+  
 
   const config = new DocumentBuilder()
     .setTitle('Tadreeby API')
