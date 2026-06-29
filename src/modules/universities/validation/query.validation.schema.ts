c30641af0a222c7e5c32b467e3fe96ccf91a9830
shahd <eng.shahd.abusharif@gmail.com>
2026-06-29 15:31:23 +0300

    Move university module to Organization folder

--- src/modules/universities/validation/query.validation.schema.ts
+++
@@ -1,8 +0,0 @@
-import { PaginationQueryType } from "src/common/types/unifiedType.types";
-import { z, ZodType } from "zod";
-
-
-export const PaginationQuerySchema = z.object({
-    page: z.coerce.number().int().positive().optional().default(1),
-    limit: z.coerce.number().int().positive().max(100).optional().default(10),
-}).strict() satisfies ZodType<PaginationQueryType>
