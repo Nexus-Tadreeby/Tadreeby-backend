b7a9cba270f8eaaa49602390e20121b0039bf511
shahd <eng.shahd.abusharif@gmail.com>
2026-07-05 16:30:49 +0300

    enhance filter for university

--- src/modules/organization/universities/validation/university-query.validation.ts
+++ src/modules/organization/universities/validation/university-query.validation.ts
@@ -4,7 +4,17 @@ export const UniversityQuerySchema = z.object({
     page: z.coerce.number().int().positive().default(1),
     limit: z.coerce.number().int().positive().max(100).default(10),
     search: z.string().optional(),
-    isActive: z.coerce.boolean().optional(),
+    // isActive: z.coerce.boolean().optional(),
+    isActive: z.preprocess(
+        (val) => {
+            if (val === 'true') return true;
+            if (val === 'false') return false;
+            if (val === true) return true;
+            if (val === false) return false;
+            return undefined;
+        },
+        z.boolean().optional()
+    ),
     location: z.string().optional(),
     phone: z.string().optional(),
     sortBy: z.enum(['name', 'createdAt', 'updatedAt']).default('createdAt'),
